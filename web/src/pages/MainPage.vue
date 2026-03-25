656420df0613cbc792d767607780a965b07cb87c
xant1ko <semasavasa@gmail.com>
2026-03-25 22:57:51 +0700

    feat: some some idk

--- web/src/pages/MainPage.vue
+++ web/src/pages/MainPage.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { useTasksStore } from "@/stores/tasks";
+</script>
+
 <template>
   <v-container>
     <div class="ga-4 mb-4 d-flex justify-space-between align-center">
@@ -9,5 +13,11 @@
       />
       <v-btn class="secondary" icon="mdi-plus" size="large" variant="text" />
     </div>
+    <section id="first-plan-priority">
+      <v-card class="ma-4 pa-4" v-for="task in useTasksStore().tasks">
+        <h3>{{ task.title }}</h3>
+        <p>{{ task.description }}</p>
+      </v-card>
+    </section>
   </v-container>
 </template>
--- web/src/utils/types.ts
+++ web/src/utils/types.ts
@@ -1,32 +1,9 @@
-export type Speciality = {
-  uid?: string
-  name: string
-  code: string
-  created_date?: string
-}
+export type Task = {
 
-export type Organization = {
-  uid?: string
-  rosminzdrav_uid: string
-  name: string
-  shortname: string
-  abbreviation: string
-  created_date?: string
+  title: string,
+  description: string,
+  dateCompleted: string,
+  priority: 'firstPlan' | 'secondplan' | 'longDistance'
 
-}
 
-export type OpinionForm = {
-  uid?: string
-  items: string
-  created_date?: string
-}
-
-export type Station = {
-  name: string
-  description: string
-  is_archived: boolean
-  uid?: string
-  created_date?: string
-  speciality_uids?: string[]
-  specialities?: Speciality[]
 }
