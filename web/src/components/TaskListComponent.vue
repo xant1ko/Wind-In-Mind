5ee91c311aa77ff77116b435e7c76c136913879d
xant1ko <semasavasa@gmail.com>
2026-04-21 17:42:11 +0700

    merge

--- web/src/components/TaskListComponent.vue
+++ web/src/components/TaskListComponent.vue
@@ -14,8 +14,9 @@
       <v-list-group class="pa-4" value="fuck">
         <div class="d-flex-column">
           <v-card
-            v-for="task in items"
-            class="pa-4 d-flex justify-space-between align-center"
+          v-for="task in items"
+          :class="getBorderColor(task.priority)"
+            class="pa-4 d-flex justify-space-between align-start"
           >
             <div>
               <h3>{{ task.title }}</h3>
@@ -23,7 +24,7 @@
               <p>{{ task.description }}</p>
             </div>
             <div class="d-flex ga-1">
-              <v-btn class="red" icon="mdi-check"></v-btn>
+              <v-btn icon="mdi-check"></v-btn>
               <v-btn icon="mdi-arrow-right"></v-btn>
             </div>
           </v-card>
@@ -43,4 +44,15 @@ defineProps<{
 }>();
 
 const isOpen = ref(["fuck"]);
+
+function getBorderColor(priority: string) {
+  if (priority === 'firstPlan') {
+    return 'borderFirstPlan'
+  } else if (priority === 'secondplan') {
+    return 'borderSecondplan'
+  } else if (priority === 'longDistance') {
+    return 'borderLongDistance'
+  }
+
+}
 </script>
